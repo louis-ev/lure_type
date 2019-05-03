a09350fa3354fd3d40bfb0df2290881109c769fe
Louis Eveillard <barlou15+github@gmail.com>
2019-05-03 11:09:28 +0200

    init with p5.vue.js

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Sketch />
+  </div>
+</template>
+
+<script>
+import Sketch from "./components/Sketch.vue";
+
+export default {
+  name: "app",
+  components: {
+    Sketch
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  font-family: sans-serif;
+
+  line-height: 1.5;
+  background-color: white;
+  color: black;
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Sketch.vue
@@ -0,0 +1,43 @@
+<template>
+  <div ref="sketch"></div>
+</template>
+<script>
+import p5 from "p5";
+
+export default {
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {
+    this.startP5Sketch();
+  },
+  beforeDestroy() {},
+  watch: {},
+  computed: {},
+  methods: {
+    startP5Sketch() {
+      console.log(`Sketch / methods: startP5Sketch`);
+
+      let sketch = p => {
+        p.preload = () => {};
+        p.setup = () => {
+          p.background(`#FF6A55`);
+        };
+        p.draw = () => {
+          p.fill(`white`);
+          p.noStroke();
+          p.ellipse(50, 50, 25, 25);
+        };
+      };
+
+      let sketch_element = this.$refs.sketch;
+      sketch_element.innerHTML = "";
+      this.sketch = new p5(sketch, sketch_element);
+    }
+  }
+};
+</script>
+<style></style>
